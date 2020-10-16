62e2a07c3f0d1b60ca9aa32a972fc1cc73f2c8cc
Target Architecture <targetarchitecture@users.noreply.github.com>
2020-10-16 23:26:32 +0100

    updates

---
+++ ESP32/src/credentials template.h
@@ -0,0 +1,9 @@
+
+const char *ssid = "";
+const char *password = "";
+
+const char *MQTT_SERVER = "";
+const char *MQTT_CLIENTID = "";
+const char *MQTT_USERNAME = "";
+const char *MQTT_KEY = "";
+const char *MQTT_TOPIC = "";
--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-//#include "credentials.h"
+#include "credentials.h"
 #include <WiFi.h>
 #include <Wire.h>
 #include "SSD1306.h"
@@ -7,19 +7,19 @@
 
 #define LED_BUILTIN 2
 
-// Set the OLED parameters
-// #define SCREEN_WIDTH 128
-// #define SCREEN_HEIGHT 64
-
+WiFiClient client;
 SSD1306 display(0x3c, 21, 22);
 QueueHandle_t queue;
-TaskHandle_t Task1;
+TaskHandle_t displayMessageTask;
+PubSubClient MQTTClient;
 
 unsigned long messageStartTime;
 unsigned long messageCount = 0;
 unsigned long maxMessageCount = 0; //874 seems to be it (with OLED display this is down to 402), (770 if not writing to serial and writing to OLED every second)
 
-void loop1(void *parameter);
+void displayMessage(void *parameter);
+void sendMQTTmessage(String message);
+void updateMessageCount();
 
 void setup()
 {
@@ -31,7 +31,7 @@ void setup()
   // Init I2C bus & OLED
   Wire.begin();
 
-  //Serial.begin(115200);
+  Serial.begin(115200);
   Serial2.begin(115200);
 
   //turn off bluetooth
@@ -39,28 +39,86 @@ void setup()
 
   if (!display.init())
   {
-    //Serial.println(F("SSD1306 allocation failed"));
+    Serial.println(F("SSD1306 allocation failed"));
     delay(10000);
     ESP.restart();
   }
 
-  // Create the queue with 5 slots of 2 bytes
+  display.flipScreenVertically();
+  display.invertDisplay();
+
+  //WIFI start up
+  Serial.printf("Connecting to %s\n", ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  //connect
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println(".");
+  }
+
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //set this to be a large enough value to allow an MQTT message containing a 22Kb JPEG to be sent
+  MQTTClient.setBufferSize(30000);
+
+  Serial.println("Connecting to MQTT server");
+  MQTTClient.setClient(client);
+  MQTTClient.setServer(MQTT_SERVER, 1883);
+
+  Serial.println("connect mqtt...");
+  if (MQTTClient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_KEY))
+  {
+    Serial.println("Connected to MQTT server");
+  }
+
+  // Create the queue with
   queue = xQueueCreate(5, sizeof(String));
 
   xTaskCreatePinnedToCore(
-      loop1,   /* Function to implement the task */
-      "Task1", /* Name of the task */
-      1000,    /* Stack size in words */
-      NULL,    /* Task input parameter */
-      0,       /* Priority of the task */
-      &Task1,  /* Task handle. */
-      0);      /* Core where the task should run */
-
+      displayMessage,      /* Function to implement the task */
+      "Display Message",   /* Name of the task */
+      5000,                /* Stack size in words */
+      NULL,                /* Task input parameter */
+      2,                   /* Priority of the task */
+      &displayMessageTask, /* Task handle. */
+      1);                  /* Core where the task should run */
 
   messageStartTime = millis();
 }
 
 void loop()
+{
+  updateMessageCount();
+
+  if (Serial2.available())
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    auto message = Serial2.readStringUntil('\n');
+
+    Serial.println(message);
+
+    sendMQTTmessage(message);
+
+    messageCount++;
+
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  if (!MQTTClient.connected())
+  {
+    if (MQTTClient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_KEY))
+    {
+      Serial.println("Reconnected");
+    }
+  }
+}
+
+void updateMessageCount()
 {
   //send reciever state every one second
   if (millis() - messageStartTime >= 1000)
@@ -70,49 +128,44 @@ void loop()
       maxMessageCount = messageCount;
     }
     messageStartTime = millis();
-    //Serial.print(F("Max Messages Recieved: "));
-    //Serial.println();
+    //Serial.print("Max Messages Recieved: " + (String)maxMessageCount);
+    Serial.println("Max Messages Recieved: " + (String)maxMessageCount);
 
-    String message = "Max:" + (String)maxMessageCount;
+    String message = "Max: " + (String)maxMessageCount;
 
-    display.clear();
-    display.drawString(0, 0, message);
-    display.display();
+    xQueueSend(queue, &message, portMAX_DELAY);
 
     messageCount = 0;
   }
-
-  if (Serial2.available())
-  {
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    auto character = Serial2.readStringUntil('\n');
-
-    //Serial.println(character);
-
-    messageCount++;
-
-    digitalWrite(LED_BUILTIN, LOW);
-  }
 }
 
-void loop1(void *parameter)
+void displayMessage(void *parameter)
 {
   for (;;)
   {
-    // Get the number of flashes required
-    // int flashTotal;
-    // xQueueReceive(queue, &flashTotal, portMAX_DELAY);
+    String message;
+    xQueueReceive(queue, &message, portMAX_DELAY);
 
-    // Serial.println("Worker - reading " + String(flashTotal));
+    Serial.println("Worker - reading " + message);
 
-  display.init();
-  display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_24);
-  display.drawString(0, 0, "Hello World");
-  display.display();
+    display.clear();
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 0, message);
+    display.display();
+  }
+}
 
-    // Slow motion delay here
-    //delay(1000);
+void sendMQTTmessage(String message)
+{
+  if (MQTTClient.connected())
+  {
+    //MQTTClient.publish(MQTT_TOPIC, fb->buf, fb_len);
+  }
+  else
+  {
+    if (MQTTClient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_KEY))
+    {
+      Serial.println("Reconnected");
+    }
   }
 }
