755618e7d6e4abb22b429179c429c5df70d9cece
Target Architecture <targetarchitecture@users.noreply.github.com>
2020-10-21 00:32:38 +0100

    tidy up

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -8,13 +8,15 @@
 #include <Adafruit_SSD1306.h>
 
 #define LED_BUILTIN 2
+//#define USE_SERIAL
 
 WiFiClient client;
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
-// QueueHandle_t messageQueue;
-// TaskHandle_t displayMessageTask;
 PubSubClient MQTTClient;
 
+//std::map<std::string, unsigned long> counters;
+unsigned long leftTriggerTime = millis();
+unsigned long rightTriggerTime = millis();
 unsigned long messageStartTime;
 unsigned long messageCount = 0;
 unsigned long maxMessageCount = 0; //874 seems to be it (with OLED display this is down to 402), (770 if not writing to serial and writing to OLED every second)
@@ -31,41 +33,40 @@ void setup()
 
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // Init I2C bus & OLED
-  Wire.begin();
-
-  Serial.begin(115200);
-  Serial2.begin(115200);
-
   //turn off bluetooth
   btStop();
 
-  // Create the queues with
-  //messageQueue = xQueueCreate(2, sizeof(String));
+#if defined(USE_SERIAL)
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+#endif
 
-  if (display.begin(SSD1306_SWITCHCAPVCC, 0x3C) == false)
+  Serial2.begin(115200);
+
+  // Init I2C bus & OLED
+  Wire.begin();
+
+  if (display.begin(SSD1306_SWITCHCAPVCC, 0x3C, false, true) == false)
   {
-    delay(1000);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(5000);
     ESP.restart();
   }
-  else
-  {
-    display.clearDisplay(); //for Clearing the display
-    //https://javl.github.io/image2cpp/
-    display.drawBitmap(0, 16, xboxLogo, 128, 48, WHITE); // display.drawBitmap(x position, y position, bitmap data, bitmap width, bitmap height, color)
-    display.display();
-  }
+
+  display.clearDisplay(); //for Clearing the display
+  //https://javl.github.io/image2cpp/
+  display.drawBitmap(0, 16, xboxLogo, 128, 48, WHITE); // display.drawBitmap(x position, y position, bitmap data, bitmap width, bitmap height, color)
+  display.display();
 
   //WIFI start up
-  displayMessage("Connecting to " + (String)ssid);
-  //Serial.printf("Connecting to %s\n", ssid);
+  displayMessage("Connecting to WiFi"); // + (String)ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   //connect
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
+    delay(1000);
     displayMessage(".");
   }
 
@@ -95,8 +96,6 @@ void loop()
 
     auto message = Serial2.readStringUntil('\n');
 
-   Serial.println(message);
-
     sendMQTTmessage(message);
 
     messageCount++;
@@ -114,13 +113,10 @@ void updateMessageCount()
     {
       maxMessageCount = messageCount;
     }
+
     messageStartTime = millis();
-    //Serial.print("Max Messages Recieved: " + (String)maxMessageCount);
+
     displayMessage("Max Msg Recieved: " + (String)maxMessageCount);
-
-    //String message = "Max: " + (String)maxMessageCount;
-
-    // displayMessage(message);
 
     messageCount = 0;
   }
@@ -128,29 +124,149 @@ void updateMessageCount()
 
 void displayMessage(String message)
 {
+#if defined(USE_SERIAL)
   Serial.println(message);
+#endif
 
   display.clearDisplay(); //for Clearing the display
-
-  //https://javl.github.io/image2cpp/
-
   display.setTextSize(1); // Draw 2X-scale text
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-
   display.print((String)message);
 
   display.drawBitmap(0, 16, xboxLogo, 128, 48, WHITE); // display.drawBitmap(x position, y position, bitmap data, bitmap width, bitmap height, color)
-  display.display();
+  display.display();                                   //https://javl.github.io/image2cpp/
 }
 
 void sendMQTTmessage(String message)
 {
   checkMQTTconnection();
 
+  //deal with the message and make ready for sending
   message.trim();
+  message.toLowerCase();
 
-  MQTTClient.publish("value", message.c_str());
+  auto mqttMessage = message.c_str();
+  auto tempValue = message.substring(message.indexOf(':') + 1);
+  auto mqttValue = tempValue.c_str();
+
+  if (message == F("click: up"))
+  {
+    MQTTClient.publish("XBOX360/1/D-Pad/Up", "Click");
+  }
+  if (message == F("click: down"))
+  {
+    MQTTClient.publish("XBOX360/1/D-Pad/Down", "Click");
+  }
+  if (message == F("click: left"))
+  {
+    MQTTClient.publish("XBOX360/1/D-Pad/Left", "Click");
+  }
+  if (message == F("click: right"))
+  {
+    MQTTClient.publish("XBOX360/1/D-Pad/Up", "Click");
+  }
+
+  // if (message == F("press: r1"))
+  // {
+  //   MQTTClient.publish("XBOX360/1/Bumper/Right", "Press");
+  // }
+
+  // if (message == F("press: l1"))
+  // {
+  //   MQTTClient.publish("XBOX360/1/Bumper/Left", "Press");
+  // }
+
+  if (message == F("click: l1"))
+  {
+    MQTTClient.publish("XBOX360/1/Bumper/Left", "Click");
+  }
+
+  if (message == F("click: r1"))
+  {
+    MQTTClient.publish("XBOX360/1/Bumper/Right", "Click");
+  }
+
+  if (message == F("click: a"))
+  {
+    MQTTClient.publish("XBOX360/1/Button/A", "Click");
+  }
+  if (message == F("click: b"))
+  {
+    MQTTClient.publish("XBOX360/1/Button/B", "Click");
+  }
+  if (message == F("click: x"))
+  {
+    MQTTClient.publish("XBOX360/1/Button/X", "Click");
+  }
+  if (message == F("click: y"))
+  {
+    MQTTClient.publish("XBOX360/1/Button/Y", "Click");
+  }
+
+  if (message == F("click: xbox"))
+  {
+    MQTTClient.publish("XBOX360/1/Button/XBOX", "Click");
+  }
+  if (message == F("click: back"))
+  {
+    MQTTClient.publish("XBOX360/1/Button/Back", "Click");
+  }
+  if (message == F("click: start"))
+  {
+    MQTTClient.publish("XBOX360/1/Button/Start", "Click");
+  }
+  if (message == F("click: sync"))
+  {
+    MQTTClient.publish("XBOX360/1/Button/SYNC", "Click");
+  }
+
+  if (message.startsWith(F("battery:")))
+  {
+    MQTTClient.publish("XBOX360/1/Battery", mqttValue);
+  }
+
+  if (message.startsWith(F("l2:")))
+  {
+    //if time between triggers is greater than 100 milliseconds then send
+    if (millis() - leftTriggerTime > 200)
+    {
+      leftTriggerTime = millis();
+
+      long trigger = atol(mqttValue);
+
+      auto mappedValue = String(map(trigger, 0, 255, 0, 100));
+
+      MQTTClient.publish("XBOX360/1/Trigger/Left", mappedValue.c_str());
+    }
+  }
+
+  //if time between triggers is greater than 500 milliseconds then send zero
+  if (millis() - leftTriggerTime > 500)
+  {
+    leftTriggerTime = millis();
+    MQTTClient.publish("XBOX360/1/Trigger/Left", "0");
+  }
+
+  if (message.startsWith(F("r2:")))
+  {
+    //if time between triggers is greater than 100 milliseconds then send
+    if (millis() - rightTriggerTime > 200)
+    {
+      rightTriggerTime = millis();
+
+      auto mappedValue = String(map(atol(mqttValue), 0, 255, 0, 100));
+
+      MQTTClient.publish("XBOX360/1/Trigger/Right", mappedValue.c_str());
+    }
+  }
+
+  //if time between triggers is greater than 500 milliseconds then send zero
+  if (millis() - rightTriggerTime > 500)
+  {
+    rightTriggerTime = millis();
+    MQTTClient.publish("XBOX360/1/Trigger/Right", "0");
+  }
 }
 
 void checkMQTTconnection()
@@ -159,172 +275,7 @@ void checkMQTTconnection()
   {
     if (MQTTClient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_KEY))
     {
-     displayMessage("Connected to MQTT server");
+      displayMessage("Connected to MQTT server");
     }
   }
 }
-
-
-
-
-// void loopPS2(byte vibrate)
-// {
-//     /* You must Read Gamepad to get new values and set vibration values
-//      ps2x.read_gamepad(small motor on/off, larger motor strength from 0-255)
-//      if you don't enable the rumble, use ps2x.read_gamepad(); with no values
-//      You should call this at least once a second
-//    */
-
-//     if (vibrate > 0)
-//     {
-//         publishMQTTmessage("Vibrate set to " + String(vibrate));
-//     }
-
-//     //DualShock Controller
-//     ps2x.read_gamepad(false, vibrate); //read controller and set large motor to spin at 'vibrate' speed
-
-//     long left_x_mapped = map(ps2x.Analog(PSS_LX), 0, 255, -100, 100);
-//     long left_y_mapped = map(ps2x.Analog(PSS_LY), 0, 255, -100, 100);
-//     long right_x_mapped = map(ps2x.Analog(PSS_RX), 0, 255, -100, 100);
-//     long right_y_mapped = map(ps2x.Analog(PSS_RY), 0, 255, -100, 100);
-
-//     //Serial.print("PSS_LX:" + String(left_x_mapped) + " PSS_LY:" + String(left_y_mapped));
-//     //Serial.println("PSS_RX:" + String(right_x_mapped) + " PSS_RY:" + String(right_y_mapped));
-
-//     // lastCommandSentMillis
-
-//     bool sendMessage = false;
-
-//     //check for centered zone and ignore
-//     if (abs(left_x_mapped) > 5 ||
-//         abs(left_y_mapped) > 5)
-//     {
-//         joystick["left_x_mapped"] = left_x_mapped;
-//         joystick["left_y_mapped"] = left_y_mapped;
-
-//         joystick["left_x"] = ps2x.Analog(PSS_LX);
-//         joystick["left_y"] = ps2x.Analog(PSS_LY);
-//     }
-
-//     //check for centered zone and ignore
-//     if (abs(right_x_mapped) > 5 ||
-//         abs(right_y_mapped) > 5)
-//     {
-//         joystick["right_x_mapped"] = right_x_mapped;
-//         joystick["right_y_mapped"] = right_y_mapped;
-
-//         joystick["right_x"] = ps2x.Analog(PSS_RX);
-//         joystick["right_y"] = ps2x.Analog(PSS_RY);
-//     }
-
-//     //only set if changed - so store the STATE !
-
-//     if (ps2x.Button(PSB_START) != joystick["start"])
-//     {
-//         joystick["start"] = ps2x.Button(PSB_START);
-//     }
-
-//     if (ps2x.Button(PSB_SELECT) == true)
-//     {
-//         joystick["ps2_select"] = ps2x.Button(PSB_SELECT);
-//     }
-
-//     if (ps2x.Button(PSB_PAD_UP) == true)
-//     {
-//         joystick["pad_up"] = ps2x.Button(PSB_PAD_UP);
-//     }
-
-//     if (ps2x.Button(PSB_PAD_DOWN) == true)
-//     {
-//         joystick["pad_down"] = ps2x.Button(PSB_PAD_DOWN);
-//     }
-
-//     if (ps2x.Button(PSB_PAD_LEFT) == true)
-//     {
-//         joystick["pad_left"] = ps2x.Button(PSB_PAD_LEFT);
-//     }
-
-//     if (ps2x.Button(PSB_PAD_RIGHT) == true)
-//     {
-//         joystick["pad_right"] = ps2x.Button(PSB_PAD_RIGHT);
-//     }
-
-//     if (ps2x.Button(PSB_TRIANGLE) == true)
-//     {
-//         joystick["action_up"] = ps2x.Button(PSB_TRIANGLE);
-//     }
-
-//     if (ps2x.Button(PSB_CROSS) == true)
-//     {
-//         joystick["action_down"] = ps2x.Button(PSB_CROSS);
-//     }
-
-//     if (ps2x.Button(PSB_SQUARE) == true)
-//     {
-//         joystick["action_left"] = ps2x.Button(PSB_SQUARE);
-//     }
-
-//     if (ps2x.Button(PSB_CIRCLE) == true)
-//     {
-//         joystick["action_right"] = ps2x.Button(PSB_CIRCLE);
-//     }
-
-//     if (ps2x.Button(PSB_L1) == true)
-//     {
-//         joystick["shoulder_left"] = ps2x.Button(PSB_L1);
-//     }
-
-//     if (ps2x.Button(PSB_R1) == true)
-//     {
-//         joystick["shoulder_right"] = ps2x.Button(PSB_R1);
-//     }
-
-//     if (ps2x.Button(PSB_L2) == true)
-//     {
-//         joystick["trigger_left"] = ps2x.Button(PSB_L2);
-//     }
-
-//     if (ps2x.Button(PSB_R2) == true)
-//     {
-//         joystick["trigger_right"] = ps2x.Button(PSB_R2);
-//     }
-
-//     if (ps2x.Button(PSB_L3) == true)
-//     {
-//         joystick["left"] = ps2x.Button(PSB_L3);
-//     }
-
-//     if (ps2x.Button(PSB_R3) == true)
-//     {
-//         joystick["right"] = ps2x.Button(PSB_R3);
-//     }
-
-//     // joystick["xbox360_back"] = false;
-//     // joystick["xbox360_guide"] = false;
-//     // joystick["trigger_left_analog"] = 0;
-//     // joystick["trigger_right_analog"] = 0;
-//     //}
-
-//     JsonObject joystickObj = joystick.as<JsonObject>();
-
-//     // size_t joystickObjSize = joystickObj.size();
-//     // Serial.println(joystickObjSize);
-
-//     if (joystickObj.size() > 0)
-//     {
-//         joystick["dial"] = dial;
-//         joystick["make"] = "PS2";
-
-//         String json;
-
-//         serializeJson(joystick, json);
-
-//         //Serial.println(json);
-
-//         publishMQTTmessage(json);
-
-//         digitalWrite(LED_BUILTIN, LOW); //set LED to flash on
-
-//         lastCommandSentMillis = millis(); //reset timer
-//     }
-// }
--- README.md
+++ README.md
@@ -12,49 +12,59 @@ A convoluted solution involving an Arduino Uno, USB host controller v1.3 and an 
     <th>MQTT Value</th>
   </tr>
 
-<tr><td>Battery:</td><td>0-3</td><td>XBOX360/Battery</td><td>0/3</td></tr>
-<tr><td>L2:</td><td>0-255</td><td>XBOX360/Trigger/Left</td><td>0/100</td></tr>
-<tr><td>R2:</td><td>0-255</td><td>XBOX360/Trigger/Right</td><td>0/100</td></tr>
+<tr><td>Battery:</td><td>0-3</td><td>XBOX360/1/Battery</td><td>0/3</td></tr>
+<tr><td>L2:</td><td>0-255</td><td>XBOX360/1/Trigger/Left</td><td>0/100</td></tr>
+<tr><td>R2:</td><td>0-255</td><td>XBOX360/1/Trigger/Right</td><td>0/100</td></tr>
 
-<tr><td>LeftHatX:</td><td>-32768/32767</td><td>XBOX360/Stick/Left/X</td><td>-100/100</td></tr>
-<tr><td>LeftHatY:</td><td>-32768/32767</td><td>XBOX360/Stick/Left/Y</td><td>-100/100</td></tr>
-<tr><td>Click: L3</td><td></td><td>XBOX360/Stick/Left</td><td>Click</td></tr>
+<tr><td>LeftHatX:</td><td>-32768/32767</td><td>XBOX360/1/Stick/Left/X</td><td>-100/100</td></tr>
+<tr><td>LeftHatY:</td><td>-32768/32767</td><td>XBOX360/1/Stick/Left/Y</td><td>-100/100</td></tr>
+<tr><td>Click: L3</td><td></td><td>XBOX360/1/Stick/Left</td><td>Click</td></tr>
 
-<tr><td>RightHatX:</td><td>-32768/32767</td><td>XBOX360/Stick/Right/X</td><td>-100/100</td></tr>
-<tr><td>RightHatY:</td><td>-32768/32767</td><td>XBOX360/Stick/Right/Y</td><td>-100/100</td></tr>
-<tr><td>Click: R3</td><td></td><td>XBOX360/Stick/Right</td><td>Click</td></tr>
+<tr><td>RightHatX:</td><td>-32768/32767</td><td>XBOX360/1/Stick/Right/X</td><td>-100/100</td></tr>
+<tr><td>RightHatY:</td><td>-32768/32767</td><td>XBOX360/1/Stick/Right/Y</td><td>-100/100</td></tr>
+<tr><td>Click: R3</td><td></td><td>XBOX360/1/Stick/Right</td><td>Click</td></tr>
 
-<tr><td>Click: Up</td><td></td><td>XBOX360/D-Pad/Up</td><td>Click</td></tr>
-<tr><td>Press: Up</td><td></td><td>XBOX360/D-Pad/Up</td><td>Press</td></tr>
-<tr><td>Click: Down</td><td></td><td>XBOX360/D-Pad/Down</td><td>Click</td></tr>
-<tr><td>Press: Down</td><td></td><td>XBOX360/D-Pad/Down</td><td>Press</td></tr>
-<tr><td>Click: Left</td><td></td><td>XBOX360/D-Pad/Left</td><td>Click</td></tr>
-<tr><td>Press: Left</td><td></td><td>XBOX360/D-Pad/Left</td><td>Press</td></tr>
-<tr><td>Click: Right</td><td></td><td>XBOX360/D-Pad/Right</td><td>Click</td></tr>
-<tr><td>Press: Right</td><td></td><td>XBOX360/D-Pad/Right</td><td>Press</td></tr>
+<tr><td>Click: Up</td><td></td><td>XBOX360/1/D-Pad/Up</td><td>Click</td></tr>
+<tr><td>Press: Up</td><td></td><td>XBOX360/1/D-Pad/Up</td><td>Press</td></tr>
+<tr><td>Click: Down</td><td></td><td>XBOX360/1/D-Pad/Down</td><td>Click</td></tr>
+<tr><td>Press: Down</td><td></td><td>XBOX360/1/D-Pad/Down</td><td>Press</td></tr>
+<tr><td>Click: Left</td><td></td><td>XBOX360/1/D-Pad/Left</td><td>Click</td></tr>
+<tr><td>Press: Left</td><td></td><td>XBOX360/1/D-Pad/Left</td><td>Press</td></tr>
+<tr><td>Click: Right</td><td></td><td>XBOX360/1/D-Pad/Right</td><td>Click</td></tr>
+<tr><td>Press: Right</td><td></td><td>XBOX360/1/D-Pad/Right</td><td>Press</td></tr>
 
-<tr><td>Click: Start</td><td></td><td>XBOX360/Button/Start</td><td>Click</td></tr>
-<tr><td>Click: Back</td><td></td><td>XBOX360/Button/Back</td><td>Click</td></tr>
-<tr><td>Click: XBOX</td><td></td><td>XBOX360/Button/XBOX</td><td>Click</td></tr>
-<tr><td>Click: SYNC</td><td></td><td>XBOX360/Button/SYNC</td><td>Click</td></tr>
+<tr><td>Click: Start</td><td></td><td>XBOX360/1/Button/Start</td><td>Click</td></tr>
+<tr><td>Click: Back</td><td></td><td>XBOX360/1/Button/Back</td><td>Click</td></tr>
+<tr><td>Click: XBOX</td><td></td><td>XBOX360/1/Button/XBOX</td><td>Click</td></tr>
+<tr><td>Click: SYNC</td><td></td><td>XBOX360/1/Button/SYNC</td><td>Click</td></tr>
  
-<tr><td>Click: A</td><td></td><td>XBOX360/Button/A</td><td>Click</td></tr>
-<tr><td>Click: B</td><td></td><td>XBOX360/Button/B</td><td>Click</td></tr>
-<tr><td>Click: X</td><td></td><td>XBOX360/Button/X</td><td>Click</td></tr>
-<tr><td>Click: Y</td><td></td><td>XBOX360/Button/Y</td><td>Click</td></tr>
-<tr><td>Press: A</td><td></td><td>XBOX360/Button/A</td><td>Press</td></tr>
-<tr><td>Press: B</td><td></td><td>XBOX360/Button/B</td><td>Press</td></tr>
-<tr><td>Press: X</td><td></td><td>XBOX360/Button/X</td><td>Press</td></tr>
-<tr><td>Press: Y</td><td></td><td>XBOX360/Button/Y</td><td>Press</td></tr>
+<tr><td>Click: A</td><td></td><td>XBOX360/1/Button/A</td><td>Click</td></tr>
+<tr><td>Click: B</td><td></td><td>XBOX360/1/Button/B</td><td>Click</td></tr>
+<tr><td>Click: X</td><td></td><td>XBOX360/1/Button/X</td><td>Click</td></tr>
+<tr><td>Click: Y</td><td></td><td>XBOX360/1/Button/Y</td><td>Click</td></tr>
+<tr><td>Press: A</td><td></td><td>XBOX360/1/Button/A</td><td>Press</td></tr>
+<tr><td>Press: B</td><td></td><td>XBOX360/1/Button/B</td><td>Press</td></tr>
+<tr><td>Press: X</td><td></td><td>XBOX360/1/Button/X</td><td>Press</td></tr>
+<tr><td>Press: Y</td><td></td><td>XBOX360/1/Button/Y</td><td>Press</td></tr>
 
-<tr><td>Click: L1</td><td></td><td>XBOX360/Bumper/Left</td><td>Click</td></tr>
-<tr><td>Press: L1</td><td></td><td>XBOX360/Bumper/Left</td><td>Press</td></tr>
-<tr><td>Click: R1</td><td></td><td>XBOX360/Bumper/Right</td><td>Click</td></tr>
-<tr><td>Press: R1</td><td></td><td>XBOX360/Bumper/Right</td><td>Press</td></tr>
+<tr><td>Click: L1</td><td></td><td>XBOX360/1/Bumper/Left</td><td>Click</td></tr>
+<tr><td>Press: L1</td><td></td><td>XBOX360/1/Bumper/Left</td><td>Press</td></tr>
+<tr><td>Click: R1</td><td></td><td>XBOX360/1/Bumper/Right</td><td>Click</td></tr>
+<tr><td>Press: R1</td><td></td><td>XBOX360/1/Bumper/Right</td><td>Press</td></tr>
 
 </table>
 
 # Commands
+
+<table>
+  <tr>
+    <th>MQTT Topic</th>
+    <th>MQTT Value</th>
+    <th>Serial Command</th>
+    <th>Serial Value</th>
+  </tr>
+</table>
+
 
 setBigRumbleOn: 0-255
 setSmallRumbleOn: 0-255
